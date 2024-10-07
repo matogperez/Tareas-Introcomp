18585ddd32575e73a4afd93a26768ccf0378bf98
Matias Gonzalez Perez <161659100+matogperez@users.noreply.github.com>
2024-10-07 14:15:13 -0600

    Update ejercicio_cara.c
    Ejercicio cara con modificación de tamaño de n= 50, con ojos y con boca.

--- ejercicio_cara.c
+++ ejercicio_cara.c
@@ -3,46 +3,37 @@
 
 int main (void){
 	int i, j, ccx_1, ccy_1, ccx_2, ccxy_1, dij_1, dij_2, r;
-	int n= 100;
-	r=10;
-	ccx_1=25;
-	ccy_1=25;
-	ccx_2=ccx_1+50;
+	int n= 50;
+	r=8;
+	ccx_1=12;
+	ccy_1=12;
+	ccx_2=ccx_1+25;
 	int a[100][100];
 	for(i=0;i<n;i++){
 		for(j=0;j<n;j++){
 			dij_1=sqrt(((ccx_1-j)*(ccx_1-j))+((ccy_1-i)*(ccy_1-i)));
 			dij_2=sqrt(((ccx_2-j)*(ccx_2-j))+((ccy_1-i)*(ccy_1-i)));
-			if(dij_1<r){
-				a [i][j]= 0;
+			if((dij_1<r) || (dij_2<r)){
+				printf("   ");
 			}
 			
-			else if (dij_2<r){
-			
-				a [i][j]= 0;
-			
-			}
+            else if(((i>38)&&(i<43))&&((j>10)&&(j<40))){
+                
+                printf("   ");
+    
+
+
+            }
+            		
 			else{
 			
-				a[i][j]=0;
+				printf(" X ");
 			
-			}
-	for(i=0;i<n;i++){
-		for(j=0;j<n;j++){
-		
-			printf("%i"a[i][j])
-		
-		}
-	printf("\n");
+	        }
 	
-	}	
-		
-		
-		
-		}
 	
-	printf("\n");
-	}
+	    }	
+    printf("\n");
 
-
+    }
 }
