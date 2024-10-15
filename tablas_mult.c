07c25dfebe44b981ffbce0884d560a53edc7cb78
Matias Gonzalez Perez <161659100+matogperez@users.noreply.github.com>
2024-10-14 22:00:02 -0600

    Update tablas_mult.c
    Con correcciones

--- tablas_mult.c
+++ tablas_mult.c
@@ -2,12 +2,18 @@
 
 int main (void){
 
-	int i, j, n;
+	int i, j, n, fi;
 	for (j=0;j<11;j++){
 		for(i=0;i<10;i++){
 			n=i*(j-1);
-			if (i==0){
-				printf("%i|\t", j);
+			fi=j-1;
+			
+			if ((i==0) && ((j==0) || (j==1))){
+				
+				printf(" \t");
+			}
+			else if (i==0){
+				printf("%i|\t", fi);
 			}
 			else if(j==0){
 				printf("%i\t", i);
